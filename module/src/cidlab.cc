486d667d52c9aed326e5b6e03862fbd8048e41e5
ravi <selker.ravi@gmail.com>
2018-09-06 15:34:14 -0700

    Added function: WADDk

--- module/src/cidlab.cc
+++ module/src/cidlab.cc
@@ -5,6 +5,7 @@
 #include <functions/TTBk.h> // include Take The Best-k function class
 #include <functions/WFCP.h> // include sequential sampling function class
 #include <functions/TALLYk.h> // include TALLYk function class
+#include <functions/WADDk.h> // include WADDk function class
 
 
 namespace jags {
@@ -25,6 +26,7 @@ namespace cidlab { // start defining the module namespace
     insert(new TTBk);
     insert(new WFCP);
     insert(new TALLYk);
+    insert(new WADDk);
   }
 
   // Destructor function
